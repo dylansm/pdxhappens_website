6dc3befb073b77684745eead9455f9d1e1848f39
Dylan Smith <foliomedia@gmail.com>
2013-06-23 16:48:15 -0700

    Updated sidebar with mailchimp sign up.

--- wp-content/themes/origin_tweaked/sidebar-primary.php
+++ wp-content/themes/origin_tweaked/sidebar-primary.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Primary Sidebar Template
@@ -14,6 +15,20 @@ if ( is_active_sidebar( 'primary' ) ) : ?>
 	<?php do_atomic( 'before_sidebar_primary' ); // origin_before_sidebar_primary ?>
 
 	<div id="sidebar-primary" class="sidebar">
+
+          <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+          <style type="text/css">
+            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+          </style>
+          <div id="mc_embed_signup">
+          <form action="http://pdxhappens.us7.list-manage2.com/subscribe/post?u=13736e51f189f83e7961c8e8a&amp;id=ab846c9d95" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <label for="mce-EMAIL">Sign up for our newsletter</label>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          </form>
+          </div>
 
 		<?php do_atomic( 'open_sidebar_primary' ); // origin_open_sidebar_primary ?>
 
